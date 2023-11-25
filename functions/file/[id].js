6c452864dec4b7568135e41a446fb1bab28a515b
kobe koto <91669733+kobe-koto@users.noreply.github.com>
2023-11-25 19:46:25 +0800

    重写反代获取图片的逻辑

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -6,80 +6,87 @@ export async function onRequest(context) {  // Contents of context object
     } = context;
 
     const url = new URL(request.url);
-    const response = fetch(`https://telegra.ph/${url.pathname}${url.search}`, {
+    console.log(url.origin)
+    const BlockedNotice = 
+    (`${url.hostname}:${url.port}` === "localhost:8788"
+        ? `http://localhost:4321/notices/request_blocked.png`
+        : `${url.origin}/notices/request_blocked.png`
+    )
+
+
+    // retuen when referer !== admin page
+    if (request.headers.get("Referer") !== `${url.origin}/admin`) {
+        // if KV enabled 
+        if (env.img_url) {
+
+            // then we check the record from KV
+            const record = await env.img_url.getWithMetadata(params.id);
+            console.log("record:" + record)
+    
+            // if the Target image has been checked
+            if (record.metadata !== null) {
+
+                // if blocked 
+                if (record.metadata.ListType == "Block") {
+                    return Response.redirect(BlockedNotice, 302)
+                }
+    
+                // if AllowList enabled.
+                if (env.WhiteList_Mode == "true") {
+                    if (record.metadata.ListType !== "White") {
+                        return Response.redirect(BlockedNotice, 302)
+                    }
+                }
+    
+                // if target image is unsafe
+                if (record.metadata.Label == "adult") {
+                    return Response.redirect(BlockedNotice, 302)
+                }
+    
+            } else if (!env.ModerateContentApiKey) {
+                // if Moderate disabled & target image is first request
+                // assume that the image is passed the request
+                let time = new Date().getTime();
+                await env.img_url.put(params.id, "", {
+                    metadata: { ListType: "None", Label: "None", TimeStamp: time },
+                });
+            }
+    
+        }
+    
+
+
+        // if Moderate enabled & target image not checked
+        // then wo gonna to check the image 
+        if (env.ModerateContentApiKey) {
+    
+            let time = new Date().getTime(),
+                apikey = env.ModerateContentApiKey;
+    
+            await fetch(`https://api.moderatecontent.com/moderate/?key=${apikey}&url=https://telegra.ph/${url.pathname}${url.search}`)
+            .then( response => response.json() )
+            .then(async (response) => {
+                if (env.img_url) {
+                    await env.img_url.put(params.id, "", {
+                        metadata: { ListType: "None", Label: moderate_data.rating_label, TimeStamp: time },
+                    });
+                }
+                if (response.rating_label == "adult") {
+                    return Response.redirect(BlockedNotice, 302)
+                }
+            });
+    
+        }
+    }
+
+
+
+
+
+
+    return fetch(`https://telegra.ph/${url.pathname}${url.search}`, {
         method: request.method,
         headers: request.headers,
         body: request.body,
     })
-    .then(async (response) => {
-        if (response.ok) {
-            // Referer header equal to the admin page
-            if (request.headers.get("Referer") === `${url.origin}/admin`) {
-                //show the image
-                return response;
-            }
-
-            if (env.img_url) {
-                //check the record from kv
-                const record = await env.img_url.getWithMetadata(params.id);
-                console.log(`record: ${record}`)
-                if (record.metadata !== null) {
-                    //if the record is not null, redirect to the image
-                    if (record.metadata.ListType == "White") {
-                        return response;
-                    } else if (record.metadata.ListType == "Block" || record.metadata.Label == "adult") {
-                        if (!request.headers.get("Referer")) {
-                            return Response.redirect(`${url.origin}/notices/block-img.html`, 302)
-                        } else {
-                            return Response.redirect(`${url.origin}/notices/img-blocked.png`, 302)
-                        }
-                    }
-
-                    //check if the env variables WhiteList_Mode are set
-                    console.log("env.WhiteList_Mode:", env.WhiteList_Mode)
-                    if (env.WhiteList_Mode == "true") {
-                        //if the env variables WhiteList_Mode are set, redirect to the image
-                        return Response.redirect(`${url.origin}/notices/whitelist-on.html`, 302);
-                    } else {
-                        //if the env variables WhiteList_Mode are not set, redirect to the image
-                        return response;
-                    }
-                }
-
-            }
-
-            let time = new Date().getTime(),
-                apikey = env.ModerateContentApiKey;
-
-            if (!apikey) {
-                if (!env.img_url) {
-                    console.log("Not enbaled KV")
-                } else {
-                    //add image to kv
-                    await env.img_url.put(params.id, "", {
-                        metadata: { ListType: "None", Label: "None", TimeStamp: time },
-                    });
-                }
-            } else {
-                await fetch(`https://api.moderatecontent.com/moderate/?key=${apikey}&url=https://telegra.ph/${url.pathname}${url.search}`)
-                    .then(async (response) => {
-                        let moderate_data = await response.json();
-                        // console.log(moderate_data)
-                        // console.log("---env.img_url---")
-                        // console.log(env.img_url == "true")
-                        if (!env.img_url) {
-                            //add image to kv
-                            await env.img_url.put(params.id, "", {
-                                metadata: { ListType: "None", Label: moderate_data.rating_label, TimeStamp: time },
-                            });
-                        }
-                        if (moderate_data.rating_label == "adult") {
-                            return Response.redirect(`${url.origin}/notices/block-img.html`, 302)
-                        }
-                    });
-            }
-        }
-        return response;
-    });
-    return response;
 }
